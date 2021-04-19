c77226a6498417d5ff6259c71337eeb4ed98e325
Oleg <mastercrutor89@gmail.com>
2021-04-19 16:47:15 +0300

    create-table: Create the table with JS

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,8 +1,42 @@
+import {capitalize} from "@core/utils";
+
 export class DomListener {
-	constructor($root) {
+
+	constructor($root, listeners = []) {
+
 		if (!$root) {
 			throw new Error(`No $root provided for DomListener`)
 		}
 		this.$root = $root;
+		this.listeners = listeners;
 	}
+
+	initDOMListeners() {
+		// console.log(this.listeners)
+		this.listeners.forEach((listener) => {
+			const method = getMethodname(listener);
+			if (!this[method]) {
+				throw new Error(`Method ${method} is not implemented in ${this.name || ''} Component`)
+			}
+			// Тоже самое что и addEventListener
+			this[method] = this[method].bind(this);
+			this.$root.on(listener, this[method])
+		})
+	}
+
+	removeDOMListeners() {
+		this.listeners.forEach((listener) => {
+			const method = getMethodname(listener);
+			if (!this[method]) {
+				throw new Error(`Method ${method} is not implemented in ${this.name || ''} Component`)
+			}
+			// Тоже самое что и addEventListener
+			this.$root.off(listener, this[method])
+		})
+	}
+}
+
+
+function getMethodname(eventName){
+	return 'on' + capitalize(eventName)
 }
